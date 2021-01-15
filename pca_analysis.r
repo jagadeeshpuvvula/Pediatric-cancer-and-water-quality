1f846f99f25ff7b6500cbb2abb652e699539048a
jagadeeshpuvvula <jagadeesh.puvvula@gmail.com>
2021-01-15 14:32:42 -0600

    Update pca_analysis.r
    updates

--- pca_analysis.r
+++ pca_analysis.r
@@ -46,8 +46,8 @@ ggplot()+geom_pointrange(data=d, mapping=aes(x=Atrazine_Surface_concentration_ca
                                              y=IRR,ymin=upper, ymax=lower), width=0.2, size=1, color="black", fill="black", shape=22)
 
 
-d=data.frame(Atrazine_concentration_quantile_category=c("Medium-low","Medium","High", 
-                                                        "Medium-low","Medium","High"),
+d=data.frame(Atrazine_concentration_quantile_category=c("Low","Medium","High", 
+                                                        "Low","Medium","High"),
              IRR=c(3.56, 3.43, 4.64, 1.61,3.56,3.76),
              lower=c(1.45, 1.29, 1.81, 0.56,1.26,1.36), 
              upper=c(8.72, 9.09, 11.85, 4.6,10.03,10.37), 
@@ -58,4 +58,4 @@ ggplot()+geom_pointrange(data=d, mapping=aes(x=Atrazine_concentration_quantile_c
   scale_y_continuous(breaks=c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14))+
   geom_hline(yintercept = 1, linetype = 2)+theme(legend.position="bottom")+
   facet_grid(cols = vars(s), scales="free_x")+
-  scale_x_discrete(limits=c("Medium-low","Medium","High"))
+  scale_x_discrete(limits=c("Low","Medium","High"))
